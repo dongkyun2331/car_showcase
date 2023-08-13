8b05a4af70666f535a910b01f702e1755850f10a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-13 22:54:27 +0900

    types

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
+
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -13,7 +17,11 @@ const Hero = () => {
           간편한 예약 절차를 통해 렌터카 경험을 간소화하세요.
         </p>
 
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   );
