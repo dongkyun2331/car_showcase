bc76a18522c890104ef2a9483b048ceee96b0ae0
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-13 19:56:05 +0900

    CustomButton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          빠르고 쉽게 자동차를 찾고, 예약하고, 렌트하세요!
+        </h1>
+
+        <p className="hero__subtitle">
+          간편한 예약 절차를 통해 렌터카 경험을 간소화하세요.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
