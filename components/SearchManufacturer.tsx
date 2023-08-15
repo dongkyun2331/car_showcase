6c8cffc743ab2e6371d86a8fc6c03b01fb372387
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-15 16:59:24 +0900

    Combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,18 @@
+import { Combobox, Transition } from "@headlessui/react";
+
 import { SearchManufacturerProps } from "@/types";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full"></div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
