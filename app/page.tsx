9c999b234a0b754a95dc030c500c9da85292d2df
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-15 14:14:46 +0900

    home__filter

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,16 @@ export default function Home() {
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">자동차 카탈로그</h1>
+          <p>당신이 좋아할만한 자동차를 탐색하십시오</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
         </div>
       </div>
     </main>
