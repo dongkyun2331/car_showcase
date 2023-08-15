05255a800f0da27b95b5426d69cb897add2656fd
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-15 14:43:30 +0900

    searchmanufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import React from "react";
+import { SearchManufacturer } from "./";
 
 const SearchBar = () => {
   const handleSearch = () => {};
 
-  return <form className="searchbar" onSubmit={handleSearch}></form>;
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
