230c3031edbf0a9a2367e8d0438accae67febe1d
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-18 12:17:44 +0900

    carCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
