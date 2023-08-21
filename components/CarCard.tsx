08d5b2aedd9ef7490b25ee2f250dce21a2bea955
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-21 20:55:09 +0900

    right icon

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -14,6 +14,8 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const [isOpen, setIsOpen] = useState(false);
+
   const carRent = calculateCarRent(city_mpg, year);
 
   return (
@@ -67,6 +69,9 @@ const CarCard = ({ car }: CarCardProps) => {
           <CustomButton
             title="View More"
             containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
