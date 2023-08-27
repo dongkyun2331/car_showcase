ccfd2a800ca91a640125098f5a9e04425e81c659
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-08-28 00:59:11 +0900

    filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const CustomFilter = () => {
-  return <div>CustomFilter</div>;
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@/types";
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  return (
+    <div className="w-fit">
+      <Listbox>
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-bilter__btn">
+            <span>Filter</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default CustomFilter;
